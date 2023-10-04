a7cd78809b26e51e9410adf39e1eb9d72ba9ce30
Benjamin André-Micolon <benjamin@robocorp.com>
2023-10-04 14:13:57 +0300

    fix: logic to allow multiple mentions per keywords

--- tasks.py
+++ tasks.py
@@ -8,11 +8,43 @@ from robocorp import vault
 from robocorp import storage
 
 
-def clean_list(keyword_content):
-    keyword_content_list = keyword_content.strip().split("\n")
+def clean_list(list_of_strings):
+    keyword_content_list = list_of_strings.strip().split("\n")
     trimmed_list = [string.strip() for string in keyword_content_list]
     cleaned_list = [string for string in trimmed_list if string]
     return cleaned_list
+
+
+def get_keywords(keyword_content):
+    # Dict<keyword, { url: string; title: string }[]>
+    keywords = {}
+    current_keyword = ""
+    cleaned_list = clean_list(keyword_content)
+
+    keyword_regex = r'Keyword: "(.*?)"'
+
+    for item in cleaned_list:
+        keyword_match = re.search(keyword_regex, item)
+
+        if keyword_match:
+            keyword = keyword_match.group(1)
+            current_keyword = keyword
+            keywords[current_keyword] = []
+        else:
+            key_type = "url" if item.startswith("http") else "title"
+
+            existing_keyword_details = keywords[current_keyword]
+
+            if (
+                len(existing_keyword_details) == 0
+                or len(existing_keyword_details[-1]) == 2
+            ):
+                existing_keyword_details.append({key_type: item})
+
+            if len(existing_keyword_details[-1]) < 2:
+                existing_keyword_details[-1][key_type] = item
+
+    return keywords
 
 
 @task
@@ -29,44 +61,58 @@ def send_notification():
     subreddits_to_ignore = clean_list(storage.get_text("mentions_black_list"))
 
     keyword_content = match.group(1)
-    keyword, title, url = clean_list(keyword_content)
+
+    keywords = get_keywords(keyword_content)
 
     secret = vault.get_secret("SlackNotifier")
     slack_webhook = secret["WEBHOOK"]
     headers = {"Content-type": "application/json"}
 
-    payload = {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "\n".join([f"*{keyword}*", f"_{title}_"]),
-                },
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
+    for keyword in keywords:
+        blocks = []
+
+        for mention in keywords[keyword]:
+            url = mention["url"]
+            title = mention["title"]
+
+            if any(subreddit in url for subreddit in subreddits_to_ignore):
+                print(
+                    f"Not posting the mention: {url} is included in the list of subreddits to ignore."
+                )
+                print("Subreddits to ignore", subreddits_to_ignore)
+                continue
+
+            blocks.append(
+                {
+                    "type": "section",
+                    "text": {
                         "type": "mrkdwn",
-                        "text": url,
+                        "text": "\n".join([f"*{keyword}*", f"_{title}_"]),
                     },
-                ],
-            },
-        ],
-        "channel": os.getenv("SLACK_CHANNEL"),
-    }
+                },
+            )
+            blocks.append(
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": url,
+                        },
+                    ],
+                },
+            )
 
-    if any(subreddit in url for subreddit in subreddits_to_ignore):
-        print(
-            f"Not posting the mention: {url} is included in the list of subreddits to ignore."
+        payload = {
+            "blocks": blocks,
+            "channel": os.getenv("SLACK_CHANNEL"),
+        }
+
+        response = requests.post(
+            slack_webhook, headers=headers, data=json.dumps(payload)
         )
-        print("Subreddits to ignore", subreddits_to_ignore)
-        return
 
-    response = requests.post(slack_webhook, headers=headers, data=json.dumps(payload))
-
-    if response.status_code == 200:
-        print("Message posted successfully.")
-    else:
-        print(f"Failed to post message: {response.content}")
+        if response.status_code == 200:
+            print("Message posted successfully.")
+        else:
+            print(f"Failed to post message: {response.content}")
